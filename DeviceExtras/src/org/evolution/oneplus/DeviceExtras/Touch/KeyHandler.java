d09e83994e1661de85db9260bc624f8146c7a3d3
DroidFreak32 <rushabshah32@gmail.com>
2022-06-09 18:12:34 -0400

    Use lineage-sdk as a dependency
    Change-Id: I1618b29b53f7f35603828888705a657b02321873

--- DeviceExtras/src/org/evolution/oneplus/DeviceExtras/Touch/KeyHandler.java
+++ DeviceExtras/src/org/evolution/oneplus/DeviceExtras/Touch/KeyHandler.java
@@ -46,6 +46,8 @@ import android.view.KeyEvent;
 
 import com.android.internal.os.DeviceKeyHandler;
 
+import lineageos.providers.LineageSettings;
+
 public class KeyHandler implements DeviceKeyHandler {
 
     private static final String TAG = KeyHandler.class.getSimpleName();
@@ -236,7 +238,7 @@ public class KeyHandler implements DeviceKeyHandler {
 
     private void launchCamera() {
         mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
-        final Intent intent = new Intent(android.content.Intent.ACTION_SCREEN_CAMERA_GESTURE);
+        final Intent intent = new Intent(lineageos.content.Intent.ACTION_SCREEN_CAMERA_GESTURE);
         mContext.sendBroadcastAsUser(intent, UserHandle.CURRENT,
                 Manifest.permission.STATUS_BAR_SERVICE);
         doHapticFeedback();
@@ -324,8 +326,8 @@ public class KeyHandler implements DeviceKeyHandler {
             return;
         }
 
-        final boolean enabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK, 1) != 0;
+        boolean enabled = LineageSettings.System.getIntForUser(mContext.getContentResolver(),
+                LineageSettings.System.TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK, 1, UserHandle.USER_CURRENT) != 0;
         if (enabled) {
             mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
         }
